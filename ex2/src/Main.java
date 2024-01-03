6810877c3448eb44f26c14503178e5d13a3203f1
PedroBarge <pedro525tavares@gmail.com>
2024-01-03 10:34:36 +0000

    done ex2

---
+++ ex2/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Execise 2");
+        System.out.println("2. Write a Java program to get the state of the currently executing thread");
+        System.out.println();
+        ThreadEx2 threadEx2 = new ThreadEx2();
+        threadEx2.start();
+    }
+}
---
+++ ex2/src/ThreadEx2.java
@@ -0,0 +1,13 @@
+public class ThreadEx2 extends Thread{
+    @Override
+    public void run() {
+        System.out.println("Thread start");
+        try {
+            System.out.println("Thread.sleep(2000) is running");
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Thread done");
+    }
+}
