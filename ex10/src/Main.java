0e44fadc48273cc30d2c46d22d0c2d6bb7bae6c2
PedroBarge <pedro525tavares@gmail.com>
2024-01-03 12:00:54 +0000

    done ex6 and 14

--- ex10/src/Main.java
+++ ex10/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.TreeMap;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("10. Write a Java program to create multiple threads");
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("10. Write a Java program to create multiple threads\n");
+        System.out.println("11. Write a Java program to demonstrate the Thread.join() method\n");
+        System.out.println("12. Write a Java program to sleep a thread\n");
+
+        for (int i = 1; i < 6; i++) {
+            Thread thread = new ThreadEx10();
+            System.out.println("Thread number: " + i);
+            thread.start();
+            thread.join();
+        }
     }
 }
--- ex6/src/Main.java
+++ ex6/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("6. Write a Java program to set and print thread name\n");
+        System.out.println("14. Write a Java program to set a thread name using the setName() method\n");
 
         Thread threadEx6 = new ThreadEx6();
         threadEx6.start();
