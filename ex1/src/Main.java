a088d1ad9bfd1a928b6f51f554608a5d728f47e4
PedroBarge <pedro525tavares@gmail.com>
2024-01-03 10:46:46 +0000

    done ex4

--- ex1/src/Main.java
+++ ex1/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Exercise 1");
         System.out.println("1. Write a Java program to create a thread by extending the Thread class");
-        ThreadEx1 threadEx1 = new ThreadEx1();
+        Thread threadEx1 = new ThreadEx1();
         threadEx1.start();
     }
 }
