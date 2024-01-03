74408a5383cbc964d7672d84d6f0836f0951d4fa
PedroBarge <pedro525tavares@gmail.com>
2024-01-03 10:46:14 +0000

    correct an error

--- ex3/src/Main.java
+++ ex3/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("3. Write a Java program to check thread is alive or not");
         System.out.println();
 
-        ThreadEx3 threadEx3 = new ThreadEx3();
+        Thread threadEx3 = new ThreadEx3();
         threadEx3.start();
         System.out.println("Is the thread alive? "+threadEx3.isAlive());
     }
