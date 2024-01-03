d3365072a811fcf969039488301d8364a2d8df6a
PedroBarge <pedro525tavares@gmail.com>
2024-01-03 10:29:29 +0000

    done ex1

---
+++ ex1/src/ThreadEx1.java
@@ -0,0 +1,11 @@
+public class ThreadEx1 extends Thread{
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("This is running from a Thread");
+    }
+}
