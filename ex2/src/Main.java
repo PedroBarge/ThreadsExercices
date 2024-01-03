e8c8283a0a7a782b3df55d28ac3a42a58e5c405c
PedroBarge <pedro525tavares@gmail.com>
2024-01-03 10:46:32 +0000

    done ex4

--- ex2/src/Main.java
+++ ex2/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Execise 2");
         System.out.println("2. Write a Java program to get the state of the currently executing thread");
         System.out.println();
-        ThreadEx2 threadEx2 = new ThreadEx2();
+        Thread threadEx2 = new ThreadEx2();
         threadEx2.start();
     }
 }
